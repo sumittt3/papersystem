a0e842c4a38c867d0ae1d2010121be2c253b571c
sumittt3 <sumitthakur16000@gmail.com>
2024-10-26 11:36:29 +0530

    Update StudentDashboard.js

--- src/components/StudentDashboard.js
+++ src/components/StudentDashboard.js
@@ -1,47 +1,15 @@
-import React, { useState, useEffect } from 'react';
-import axios from 'axios';
-import { useNavigate, useParams, useLocation } from 'react-router-dom';
+import React, { useEffect, useState } from 'react';
+import { useParams, useNavigate, useLocation } from 'react-router-dom';
+import PaperGen from './PaperGen'; // Adjust the path as necessary
 
-const Profile2 = ({ setShowNavButtons }) => {
-    const [currentPassword, setCurrentPassword] = useState('');
-    const [newPassword, setNewPassword] = useState('');
-    const [confirmPassword, setConfirmPassword] = useState('');
-    const [error, setError] = useState('');
-    const [successMessage, setSuccessMessage] = useState('');
-    const [dropdownOpen, setDropdownOpen] = useState(false); // State for dropdown menu
-    const navigate = useNavigate();
-    const location = useLocation();
-
+const StudentDashboard = ({ setShowNavButtons }) => {
+    const [papers, setPapers] = useState([]);
     const { username } = useParams();
+    const location = useLocation();
     const params = new URLSearchParams(location.search);
     const email = params.get('email');
-
-    const handleChangePassword = async (event) => {
-        event.preventDefault();
-
-        try {
-            const response = await axios.post('https://papersystem.onrender.com/api/student/change-password', {
-                username,
-                currentPassword,
-                newPassword,
-            });
-
-            setSuccessMessage(response.data.message);
-            setNewPassword('');
-            setConfirmPassword('');
-            setCurrentPassword('');
-            setError('');
-            setTimeout(() => {
-                setSuccessMessage(' ');
-            }, 2000);
-        } catch (error) {
-            if (error.response && error.response.data && error.response.data.message) {
-                setError(error.response.data.message);
-            } else {
-                setError('Failed to change password. Please try again later.');
-            }
-        }
-    };
+    const navigate = useNavigate();
+    const [dropdownOpen, setDropdownOpen] = useState(false);
 
     useEffect(() => {
         setShowNavButtons(false);
@@ -54,149 +22,120 @@ const Profile2 = ({ setShowNavButtons }) => {
         navigate('/');
     };
 
-    // Function to toggle dropdown
     const toggleDropdown = () => {
-        setDropdownOpen((prev) => !prev);
+        setDropdownOpen(!dropdownOpen);
     };
 
     return (
-        <div className="min-h-screen w-screen bg-teal-800 flex flex-col">
-            <div className="bg-gray-800 text-white flex justify-between items-center px-6 py-4 shadow-lg">
-                {/* Left Side: Logo and Navigation Links */}
-                <div className="flex items-center gap-6">
+        <div className="min-h-screen w-screen bg-teal-700 flex flex-col">
+            <div className="bg-gray-800 text-white flex justify-between items-center px-4 py-2 sm:px-6 sm:py-4 shadow-lg">
+                <div className="flex items-center gap-4 sm:gap-6">
                     <div className="flex items-center gap-2 bg-gradient-to-r from-blue-300 to-green-300 p-2 rounded-lg shadow-lg">
-                        <span className="text-3xl font-extrabold text-transparent bg-clip-text bg-gradient-to-r from-blue-600 to-green-600">
+                        <span className="text-xl sm:text-3xl font-extrabold text-transparent bg-clip-text bg-gradient-to-r from-blue-600 to-green-600">
                             Question <span className="text-green-700">Craft</span>
                         </span>
                     </div>
 
-                    {/* Desktop Navigation Links */}
-                    <div className="hidden md:flex flex-row gap-6 mt-0">
-                        <a href={`/StudentDashboard/${username}?email=${email}`} className="flex items-center gap-2 text-white hover:text-gray-200 transition duration-300 text-lg font-bold">
-                            <svg className="w-6 h-6" fill="currentColor" viewBox="0 0 24 24"><path d="M12 12c2.761 0 5-2.239 5-5s-2.239-5-5-5-5 2.239-5 5 2.239 5 5 5zm0 2c-2.67 0-8 1.34-8 4v2h16v-2c0-2.66-5.33-4-8-4z" /></svg>
+                    {/* Navigation Links for larger screens */}
+                    <div className="hidden sm:flex gap-6">
+                        <a href={/StudentDashboard/${username}?email=${email}} className="text-white hover:text-gray-200 transition duration-300 text-lg font-bold flex items-center">
+                          <svg className="w-6 h-6 mr-2" fill="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                                <path d="M12 12c2.761 0 5-2.239 5-5s-2.239-5-5-5-5 2.239-5 5 2.239 5 5 5zm0 2c-2.67 0-8 1.34-8 4v2h16v-2c0-2.66-5.33-4-8-4z" />
+                            </svg>
                             Dashboard
                         </a>
-                        <a href={`/Profile2/${username}?email=${email}`} className="flex items-center gap-2 text-white hover:text-gray-200 transition duration-300 text-lg font-bold">
-                            <svg className="w-6 h-6" fill="currentColor" viewBox="0 0 24 24"><path d="M12 12c2.761 0 5-2.239 5-5s-2.239-5-5-5-5 2.239-5 5 2.239 5 5 5zm0 2c-2.67 0-8 1.34-8 4v2h16v-2c0-2.66-5.33-4-8-4z" /></svg>
+                        <a href={/Profile2/${username}?email=${email}} className="text-white hover:text-gray-200 transition duration-300 text-lg font-bold flex items-center">
+                            <svg className="w-6 h-6 mr-2" fill="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                                <path d="M12 0C5.372 0 0 5.372 0 12c0 6.627 5.372 12 12 12s12-5.373 12-12S18.628 0 12 0zm0 22C6.48 22 2 17.52 2 12S6.48 2 12 2s10 4.48 10 10-4.48 10-10 10z" />
+                                <path d="M12 6c-3.313 0-6 2.688-6 6s2.688 6 6 6 6-2.688 6-6-2.688-6-6-6zm0 10c-2.211 0-4-1.789-4-4s1.789-4 4-4 4 1.789 4 4-1.789 4-4 4z" />
+                            </svg>
                             Change Password
                         </a>
-                        <a href={`/TestCollection/${username}?email=${email}`} className="flex items-center gap-2 text-white hover:text-gray-200 transition duration-300 text-lg font-bold">
-                            <svg className="w-6 h-6" fill="currentColor" viewBox="0 0 24 24"><path d="M12 12c2.761 0 5-2.239 5-5s-2.239-5-5-5-5 2.239-5 5 2.239 5 5 5zm0 2c-2.67 0-8 1.34-8 4v2h16v-2c0-2.66-5.33-4-8-4z" /></svg>
+                        <a href={/TestCollection/${username}?email=${email}} className="text-white hover:text-gray-200 transition duration-300 text-lg font-bold flex items-center">
+                           <svg className="w-6 h-6 mr-2" fill="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                                <path d="M12 0c-6.627 0-12 5.373-12 12s5.373 12 12 12 12-5.373 12-12S18.627 0 12 0zm0 22C6.48 22 2 17.52 2 12S6.48 2 12 2s10 4.48 10 10-4.48 10-10 10z" />
+                                <path d="M12 4c-.55 0-1 .45-1 1v6H7c-.55 0-1 .45-1 1s.45 1 1 1h4v6c0 .55.45 1 1 1s1-.45 1-1v-6h4c.55 0 1-.45 1-1s-.45-1-1-1h-4V5c0-.55-.45-1-1-1z" />
+                            </svg>
                             Test Collection
                         </a>
                     </div>
                 </div>
 
-                {/* Right Side: User Info and Logout */}
-                <div className="flex items-center gap-6">
-                    <div className="hidden sm:flex items-center gap-2 group">
-                        <svg className="w-6 h-6 text-white group-hover:text-gray-200 transition duration-300" fill="currentColor" viewBox="0 0 24 24">
-                            <path d="M12 12c2.761 0 5-2.239 5-5s-2.239-5-5-5-5 2.239-5 5 2.239 5 5 5zm0 2c-2.67 0-8 1.34-8 4v2h16v-2c0-2.66-5.33-4-8-4z" />
-                        </svg>
-                        <h2 className="text-xl font-bold group-hover:text-gray-200 transition duration-300">{username}</h2>
-                    </div>
-                    <div className="sm:hidden relative">
-                        <button
-                            onClick={toggleDropdown}
-                            className="flex items-center text-white hover:text-gray-200 transition duration-300"
-                        >
-                            <svg className="w-6 h-6" fill="currentColor" viewBox="0 0 24 24">
-                                <path d="M12 16l-4-4h8z" />
-                            </svg>
-                        </button>
-
-                        {dropdownOpen && (
-                            <div className="absolute right-0 mt-2 w-48 bg-gray-800 rounded-md shadow-lg z-10">
-                                <a href={`/StudentDashboard/${username}?email=${email}`} className="flex items-center block text-left px-4 py-2 text-white hover:bg-gray-700">
-                                    <svg className="w-6 h-6 mr-2" fill="currentColor" viewBox="0 0 24 24">
-                                        <path d="M12 12c2.761 0 5-2.239 5-5s-2.239-5-5-5-5 2.239-5 5 2.239 5 5 5zm0 2c-2.67 0-8 1.34-8 4v2h16v-2c0-2.66-5.33-4-8-4z" />
-                                    </svg>
-                                    Dashboard
-                                </a>
-                                <a href={`/Profile2/${username}?email=${email}`} className="flex items-center block text-left px-4 py-2 text-white hover:bg-gray-700">
-                                    <svg className="w-6 h-6 mr-2" fill="currentColor" viewBox="0 0 24 24">
-                                        <path d="M12 0C5.372 0 0 5.372 0 12c0 6.627 5.372 12 12 12s12-5.373 12-12S18.628 0 12 0zm0 22C6.48 22 2 17.52 2 12S6.48 2 12 2s10 4.48 10 10-4.48 10-10 10z" />
-                                        <path d="M12 6c-3.313 0-6 2.688-6 6s2.688 6 6 6 6-2.688 6-6-2.688-6-6-6zm0 10c-2.206 0-4-1.794-4-4s1.794-4 4-4 4 1.794 4 4-1.794 4-4 4z" />
-                                    </svg>
-                                    Change Password
-                                </a>
-                                <a href={`/TestCollection/${username}?email=${email}`} className="flex items-center block text-left px-4 py-2 text-white hover:bg-gray-700">
-                                    <svg className="w-6 h-6 mr-2" fill="currentColor" viewBox="0 0 24 24">
-                                        <path d="M12 12c2.761 0 5-2.239 5-5s-2.239-5-5-5-5 2.239-5 5 2.239 5 5 5zm0 2c-2.67 0-8 1.34-8 4v2h16v-2c0-2.66-5.33-4-8-4z" />
-                                    </svg>
-                                    Test Collection
-                                </a>
-                                <button
-                                    onClick={handleLogout}
-                                    className="flex items-center block text-left w-full px-4 py-2 text-red-600 hover:bg-red-600 hover:text-white"
-                                >
-                                    <svg className="w-6 h-6 mr-2" fill="currentColor" viewBox="0 0 24 24">
-                                        <path d="M0 0h24v24H0z" fill="none" />
-                                        <path d="M12 7v10m5-5H2m17 0l-5-5-5 5" />
-                                    </svg>
-                                    Logout
-                                </button>
-                            </div>
-                        )}
-                    </div>
-
+                {/* Username and Logout Button for larger screens */}
+                <div className="hidden sm:flex items-center gap-4">
+                    <h2 className="text-xl font-bold">{username}</h2>
                     <button
+                        className="flex items-center bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded transition duration-300 shadow-md"
                         onClick={handleLogout}
-                        className="hidden sm:flex items-center px-4 py-2 text-white hover:bg-red-600 hover:text-white transition duration-300 rounded-lg"
                     >
+                        <svg className="w-4 h-4 mr-2" fill="currentColor" viewBox="0 0 24 24">
+                            <path d="M16 13v-2h-8v2h8zm-6-6h-2v12h2v-12zm8-4v16h-12v-16h12zm0-2h-12c-1.1 0-2 .9-2 2v16c0 1.1.9 2 2 2h12c1.1 0 2-.9 2-2v-16c0-1.1-.9-2-2-2z" />
+                        </svg>
                         Logout
                     </button>
                 </div>
+
+                {/* Dropdown Menu for Mobile Devices */}
+                <div className="relative sm:hidden">
+                    <button
+                        onClick={toggleDropdown}
+                        className="flex items-center text-white hover:text-gray-200 transition duration-300"
+                    >
+                        <svg className="w-6 h-6" fill="currentColor" viewBox="0 0 24 24">
+                            <path d="M12 16l-4-4h8z" />
+                        </svg>
+                    </button>
+
+                    {dropdownOpen && (
+                        <div className="absolute right-0 mt-2 w-48 bg-gray-800 rounded-md shadow-lg z-10">
+                            <a href={/StudentDashboard/${username}?email=${email}} className="flex items-center block text-left px-4 py-2 text-white hover:bg-gray-700">
+                                <svg className="w-6 h-6 mr-2" fill="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                                <path d="M12 12c2.761 0 5-2.239 5-5s-2.239-5-5-5-5 2.239-5 5 2.239 5 5 5zm0 2c-2.67 0-8 1.34-8 4v2h16v-2c0-2.66-5.33-4-8-4z" />
+                            </svg>
+                                Dashboard
+                            </a>
+                            <a href={/Profile2/${username}?email=${email}} className="flex items-center block text-left px-4 py-2 text-white hover:bg-gray-700">
+                                <svg className="w-6 h-6 mr-2" fill="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                                <path d="M12 0C5.372 0 0 5.372 0 12c0 6.627 5.372 12 12 12s12-5.373 12-12S18.628 0 12 0zm0 22C6.48 22 2 17.52 2 12S6.48 2 12 2s10 4.48 10 10-4.48 10-10 10z" />
+                                <path d="M12 6c-3.313 0-6 2.688-6 6s2.688 6 6 6 6-2.688 6-6-2.688-6-6-6zm0 10c-2.211 0-4-1.789-4-4s1.789-4 4-4 4 1.789 4 4-1.789 4-4 4z" />
+                            </svg>
+                                Change Password
+                            </a>
+                            <a href={/TestCollection/${username}?email=${email}} className="flex items-center block text-left px-4 py-2 text-white hover:bg-gray-700">
+                                <svg className="w-6 h-6 mr-2" fill="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                                <path d="M12 0c-6.627 0-12 5.373-12 12s5.373 12 12 12 12-5.373 12-12S18.627 0 12 0zm0 22C6.48 22 2 17.52 2 12S6.48 2 12 2s10 4.48 10 10-4.48 10-10 10z" />
+                                <path d="M12 4c-.55 0-1 .45-1 1v6H7c-.55 0-1 .45-1 1s.45 1 1 1h4v6c0 .55.45 1 1 1s1-.45 1-1v-6h4c.55 0 1-.45 1-1s-.45-1-1-1h-4V5c0-.55-.45-1-1-1z" />
+                            </svg>
+                                Test Collection
+                            </a>
+                            <button
+                                className="flex items-center block w-full text-left px-4 py-2 text-white hover:bg-gray-700"
+                                onClick={handleLogout}
+                            >
+                                <svg className="w-6 h-6 mr-2" fill="currentColor" viewBox="0 0 24 24">
+                                    <path d="M16 13v-2h-8v2h8zm-6-6h-2v12h2v-12zm8-4v16h-12v-16h12zm0-2h-12c-1.1 0-2 .9-2 2v16c0 1.1.9 2 2 2h12c1.1 0 2-.9 2-2v-16c0-1.1-.9-2-2-2z" />
+                                </svg>
+                                Logout
+                            </button>
+                        </div>
+                    )}
+                </div>
             </div>
 
-            <div className="flex justify-center items-center flex-col flex-grow">
-                <h1 className="text-4xl font-bold text-white mt-6">Change Password</h1>
-                {error && <p className="text-red-500 mt-2">{error}</p>}
-                {successMessage && <p className="text-green-500 mt-2">{successMessage}</p>}
-                <form onSubmit={handleChangePassword} className="bg-gray-700 p-6 rounded-lg shadow-md mt-6 w-80">
-                    <div className="mb-4">
-                        <label className="block text-white mb-2" htmlFor="currentPassword">Current Password</label>
-                        <input
-                            type="password"
-                            id="currentPassword"
-                            value={currentPassword}
-                            onChange={(e) => setCurrentPassword(e.target.value)}
-                            className="w-full px-3 py-2 border border-gray-300 rounded focus:outline-none focus:ring focus:ring-blue-500"
-                            required
-                        />
-                    </div>
-                    <div className="mb-4">
-                        <label className="block text-white mb-2" htmlFor="newPassword">New Password</label>
-                        <input
-                            type="password"
-                            id="newPassword"
-                            value={newPassword}
-                            onChange={(e) => setNewPassword(e.target.value)}
-                            className="w-full px-3 py-2 border border-gray-300 rounded focus:outline-none focus:ring focus:ring-blue-500"
-                            required
-                        />
-                    </div>
-                    <div className="mb-4">
-                        <label className="block text-white mb-2" htmlFor="confirmPassword">Confirm Password</label>
-                        <input
-                            type="password"
-                            id="confirmPassword"
-                            value={confirmPassword}
-                            onChange={(e) => setConfirmPassword(e.target.value)}
-                            className="w-full px-3 py-2 border border-gray-300 rounded focus:outline-none focus:ring focus:ring-blue-500"
-                            required
-                        />
-                    </div>
-                    <button
-                        type="submit"
-                        className="w-full bg-blue-600 text-white font-bold py-2 rounded hover:bg-blue-700 transition duration-300"
-                    >
-                        Change Password
-                    </button>
-                </form>
+            <div className="flex flex-col gap-4 text-white p-4 pt-6">
+                <div>
+                    <h1 className="text-3xl sm:text-5xl font-serif font-bold text-white outline-white pl-2 pt-3">
+                        Welcome back, {username}
+                    </h1>
+                </div>
+
+                {/* Render PaperGen Component */}
+                <div className="flex justify-center place-content-center mt-10">
+                    <PaperGen username={username} email={email} />
+                </div>
             </div>
         </div>
     );
 };
 
-export default Profile2;
+export default StudentDashboard; 
